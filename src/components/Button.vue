3c12d8916818ea45a20b3264175bc1694131beae
Aleksey Averianov <Flex_d1@mail.ru>
2025-06-05 15:20:32 +0300

    Добавлен компонент кнопки

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  button {
+    padding: 16px 10px;
+    border-radius: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--colorWhite);
+    font-size: 24px;
+    line-height: 36px;
+    min-width: 335px;
+    background: var(--colorBlue);
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+  button:hover {
+    background: #006FCB;
+  }
+</style>
