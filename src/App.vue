13da827579f1d690c7445f2bf09b08f9902c3e2b
Aleksey Averianov <Flex_d1@mail.ru>
2025-06-20 14:12:14 +0300

    Вывод карточек через API

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,69 @@
 <script setup>
-  import Button from "/src/components/Button.vue";
-  import Header from './components/Header.vue'
-  import Card from './components/Card.vue'
-  import { ref } from "vue";
+import Button from "/src/components/Button.vue";
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
+import { computed, onMounted, ref } from "vue";
 
-  const countValue = ref(1);
+const countValue = ref(1);
 
-  const valueCard = ref(
-    [
-      {
-        word: 'unadmitted',
-        translation: 'непризнанный',
-        status: 'pending',
-      },
-      {
-        word: 'armour-piercer',
-        translation: 'бронебойщик',
-        status: 'pending-translate',
-      },
-      {
-        word: 'stamen',
-        translation: 'тычинка',
-        status: 'sucsess',
-      },
-      {
-        word: 'vino',
-        translation: 'вино',
-        status: 'falied',
-      },
-    ]
-  )
+const valueCard = ref(null
+  // [
+  //   {
+  //     word: 'unadmitted',
+  //     translation: 'непризнанный',
+  //     status: 'pending',
+  //   },
+  //   {
+  //     word: 'armour-piercer',
+  //     translation: 'бронебойщик',
+  //     status: 'pending-translate',
+  //   },
+  //   {
+  //     word: 'stamen',
+  //     translation: 'тычинка',
+  //     status: 'sucsess',
+  //   },
+  //   {
+  //     word: 'vino',
+  //     translation: 'вино',
+  //     status: 'falied',
+  //   },
+  // ]
+)
+
+
+
+async function getCards() {
+  const response = await fetch('http://localhost:8080/api/random-words');
+  const data = await response.json();
+  valueCard.value = data.map(item => {
+    return {
+      word: item.word,
+      translation: item.translation,
+      status: 'pending',
+    }
+  })
+
+  console.log(valueCard.value);
+
+}
 
 </script>
 
 <template>
   <Header />
-  <main class="main">
-    <Button class="start-game">
+  <main v-if='valueCard === null' class="main">
+    <Button @click='getCards()' class="start-game">
       Начать игру
     </Button>
   </main>
 
-  <div class="pole-cards">
-    <Card v-for='(card, index) in valueCard' :key='index' :number='"0" + (index + 1)'
-      :label='card.word'
-      :status='card.status'
-      :translation='card.translation'
-      @resize-card='(data) => console.log(data)' @status-card='(data) => console.log(data)'
-    />
+  <div v-else class="pole-cards">
+    <Card v-for='(card, index) in valueCard' :key='index' :number='(index <= 8) ? "0" + (index + 1) : (index + 1)' :label='card.word'
+    :status='card.status' :translation='card.translation' @resize-card='(data) => console.log(data)'
+    :score="0"
+    @status-card='(data) => console.log(data)' />
+
   </div>
 
 
@@ -55,20 +71,21 @@
 </template>
 
 <style scoped>
-  .pole-cards {
-    display: grid;
-    grid-template-columns: repeat(4, minmax(0px, 1fr));
-    column-gap: 107px;
-    row-gap: 66px;
-    max-width: 1350px;
-    margin-left: auto;
-    margin-right: auto;
-  }
+.pole-cards {
+  display: grid;
+  grid-template-columns: repeat(4, minmax(0px, 1fr));
+  column-gap: 107px;
+  row-gap: 66px;
+  max-width: 1350px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
 
-  .start-game {
-    max-width: 335px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
+.start-game {
+  max-width: 335px;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
